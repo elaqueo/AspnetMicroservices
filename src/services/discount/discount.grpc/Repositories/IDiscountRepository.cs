1db5e66a09b35529d27c97aa28061d14eb61f96a
Pablo Daniel Britos <elaqueo@gmail.com>
2021-08-09 13:23:17 -0300

    discount grpc: initial commit

---
+++ src/services/discount/discount.grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using discount.grpc.Entities;
+
+namespace discount.grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
